37f4e2d69f1a1d9c1c65c5b73c5db6ea9de75b60
yuiseki <yuiseki@gmail.com>
2022-08-20 04:58:49 +0000

    カテゴリに対応

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,17 @@ import rehypeRaw from "rehype-raw";
 import { SearchQueryInput } from "./components/SearchQueryInput";
 import { useDebounce } from "./hooks/debounce";
 
-const fetcher = (url) => fetch(url).then((r) => r.json());
+const fetcher = (url: string) => fetch(url).then((r) => r.json());
+
+const categories = [
+  "life_stage_categories",
+  "personal_purpose_categories",
+  "personal_service_categories",
+  "request_categories",
+  "situation_categories",
+  "support_categories",
+  "target_categories",
+];
 
 function App() {
   const { data } = useSWR("/supports.json", fetcher);
@@ -20,11 +30,18 @@ function App() {
     if (!data) {
       return;
     }
+    const newSupports = data.items.map((support: any) => {
+      const cats = categories
+        .map((cat) => support[cat].map((c: any) => c.name))
+        .flat();
+      support.all_categories = [...new Set(cats)];
+      return support;
+    });
     if (!debouncedQuery || debouncedQuery.length === 0) {
-      setSupports(data.items);
+      setSupports(newSupports);
       return;
     }
-    const filteredSupports = data.items.filter((support) => {
+    const filteredSupports = newSupports.filter((support: any) => {
       return debouncedQuery
         .split(/[\x20\u3000]+/)
         .map((q) => {
@@ -35,7 +52,8 @@ function App() {
             support.target.includes(q) ||
             support.body.includes(q) ||
             support.governing_law.includes(q) ||
-            support.inquiry.includes(q)
+            support.inquiry.includes(q) ||
+            support.all_categories.includes(q)
           );
         })
         .every((v) => v === true);
@@ -82,7 +100,7 @@ function App() {
       >
         {supports && supports.length + "件の制度を爆速で検索"}
       </div>
-      <div style={{ width: '100%', overflowWrap: 'break-word',}}>
+      <div style={{ width: "100%", overflowWrap: "break-word" }}>
         {supports &&
           supports.map((support) => {
             return (
@@ -98,31 +116,62 @@ function App() {
                 <h3>概要</h3>
                 <p>{support.summary}</p>
                 <h3>対象</h3>
-                <p>
+                <div>
                   <ReactMarkdown
                     children={support.target}
                     remarkPlugins={[remarkGfm]}
                     rehypePlugins={[rehypeRaw]}
                   />
-                </p>
+                </div>
                 <h3>内容</h3>
-                <p>
+                <div>
                   <ReactMarkdown
                     children={support.body}
                     remarkPlugins={[remarkGfm]}
                     rehypePlugins={[rehypeRaw]}
                   />
-                </p>
-                <h3>根拠法令</h3>
-                <p>{support.governing_law}</p>
-                <h3>収録制度集</h3>
-                <p>
-                  {support.catalogs.map((catalog) => {
-                    return <p>{catalog.name}</p>;
-                  })}
-                </p>
-                <h3>問い合わせ先</h3>
-                <p>{support.inquiry}</p>
+                </div>
+                {support.governing_law && support.governing_law.length > 0 && (
+                  <>
+                    <h3>根拠法令</h3>
+                    <p>{support.governing_law}</p>
+                  </>
+                )}
+                {support.catalogs && support.catalogs.length > 0 && (
+                  <>
+                    <h3>収録制度集</h3>
+                    <div>
+                      {support.catalogs.map((catalog: any) => {
+                        return <p>{catalog.name}</p>;
+                      })}
+                    </div>
+                  </>
+                )}
+                {support.inquiry && support.inquiry.length > 0 && (
+                  <>
+                    <h3>問い合わせ先</h3>
+                    <div>
+                      <ReactMarkdown
+                        children={support.inquiry.replace(/\n/gi, "\r\n  ")}
+                        remarkPlugins={[remarkGfm]}
+                        rehypePlugins={[rehypeRaw]}
+                      />
+                    </div>
+                  </>
+                )}
+                {support.usage && support.usage.length > 0 && (
+                  <>
+                    <h3>利用方法</h3>
+                    <div>
+                      <ReactMarkdown
+                        children={support.usage.replace(/\n/gi, "\r\n  ")}
+                        remarkPlugins={[remarkGfm]}
+                        rehypePlugins={[rehypeRaw]}
+                      />
+                    </div>
+                  </>
+                )}
+                {support.all_categories.join(", ")}
               </div>
             );
           })}
