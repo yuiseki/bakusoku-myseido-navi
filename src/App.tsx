dc4d8f5844e6cf662e9366b8d075d332e3b42a99
yuiseki <yuiseki@gmail.com>
2022-08-21 07:55:33 +0000

    fix button size

--- src/App.tsx
+++ src/App.tsx
@@ -144,7 +144,7 @@ function App() {
           justifyContent: "center",
           width: "100%",
           display: "grid",
-          gridTemplateColumns: "1fr 1fr 1fr 1fr 1fr 1fr",
+          gridTemplateColumns: "1fr 1fr 1fr 1fr",
           gap: "10px",
           marginBottom: "10px",
         }}
