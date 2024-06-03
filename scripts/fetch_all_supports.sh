98b9808bb40d986acc8f63108fc3994ea3d4bcab
yuiseki <yuiseki@gmail.com>
2024-06-03 19:40:16 +0900

    correspondence to change api endpoint

--- scripts/fetch_all_supports.sh
+++ scripts/fetch_all_supports.sh
@@ -1,6 +1,6 @@
 
-endpoint="https://webapi.myseido-navi.go.jp/supports"
-params="?detail=1&get_expired=false&sort=popularity&order=desc&limit=30"
+endpoint="https://webapi.public-service.registries.go.jp/v1/supports/myseido"
+params="?accesskey=Seido202_read&detail=0&disaster=none&get_expired=false&sort=popularity&order=desc&limit=30&personal=true&municipal=false&business=false"
 url="${endpoint}${params}"
 
 echo $url
